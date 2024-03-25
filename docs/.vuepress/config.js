aae7a6efe3f1e0b2b122701f3ac4f91f716e689d
techxiaochen <techxiaochen@gmail.com>
2024-03-25 20:04:10 +0800

    first commit v1.0.0

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    title: '内部文档',
+    head:[
+        ['link', { rel: 'icon', href: '/favicon.png' }]
+    ],
+    themeConfig: {
+        logo:'/logo.png',
+        smoothScroll: true,
+        nav:[
+            {text:'主页',link:'/'},
+            {
+                text:'通知文件',
+                items: [
+                    {text:'安全生产',link: '/safeproduct/'},
+                    {text:'通报',link: '/safenote/'}
+                ] 
+            },
+            {
+                text: '安全检查',
+                items: [
+                     {text:'月度安全检查',link:'/safety-inspection/'}
+                ] 
+             },
+        ],
+        // sidebar: [
+        //     ['/safeproduct/1','泰旅爱物字1号'],
+        //     ['/safeproduct/3','泰旅爱物字3号'],
+        //     ['/safeproduct/6','泰旅爱物字6号']
+        // ]
+        // 
+        sidebar: {
+            '/safeproduct':[
+                ['/safeproduct/1','泰旅爱物字1号'],
+                ['/safeproduct/3','泰旅爱物字3号'],
+                ['/safeproduct/6','泰旅爱物字6号']
+            ],
+            '/safenote':[
+                ['/safenote/1','泰旅爱物字1号']
+            ],
+            '/safety-inspection':[
+                ['/safety-inspection/m2-f1','2月份1楼'],
+                ['/safety-inspection/m2-f2','2月份2楼']
+
+            ]
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+---
+home: true
+pageClass:
+heroImage: '/hero.jpg'
+---
